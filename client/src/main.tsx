9d9dbbcb6398a30fe3e4358ffc8b7aa889f5c35c
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2023-03-01 16:10:04 +0100

    * as

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { httpBatchLink } from "@trpc/client";
 import { useState } from "react";
