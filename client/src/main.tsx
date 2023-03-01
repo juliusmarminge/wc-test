c524b6b90aea2867d1d51c3a49ae52e2e67cd00e
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2023-03-01 14:18:16 +0100

    cleanup + automatic refetch

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,9 +1,42 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { App } from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { httpBatchLink } from "@trpc/client";
+import { useState } from "react";
+import { Greeting } from "./Greeting";
+import { trpc } from "./utils/trpc";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 );
+
+function App() {
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            refetchInterval: 1500,
+          },
+        },
+      })
+  );
+  const [trpcClient] = useState(() =>
+    trpc.createClient({
+      links: [
+        httpBatchLink({
+          url: "http://localhost:2022",
+        }),
+      ],
+    })
+  );
+  return (
+    <trpc.Provider client={trpcClient} queryClient={queryClient}>
+      <QueryClientProvider client={queryClient}>
+        <Greeting />
+      </QueryClientProvider>
+    </trpc.Provider>
+  );
+}
