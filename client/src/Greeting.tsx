88e0cb2e786de11597696d439d7c015d99b8c040
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2023-03-01 14:19:51 +0100

    some padding

--- client/src/Greeting.tsx
+++ client/src/Greeting.tsx
@@ -3,5 +3,9 @@ import { trpc } from "./utils/trpc";
 export function Greeting() {
   const greeting = trpc.greeting.useQuery({ name: "tRPC user" });
 
-  return <div className="text-zinc-200">{greeting.data?.text}</div>;
+  return (
+    <div className="text-zinc-200 p-8">
+      <h1 className="text-2xl font-bold">{greeting.data?.text}</h1>
+    </div>
+  );
 }
